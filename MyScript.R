a07a25cb2324c75d9caee81b73ecb3b259c6305d
sgaichas <sgaichas@gmail.com>
2021-06-17 10:33:31 -0400

    add example script

---
+++ MyScript.R
@@ -0,0 +1,17 @@
+2+2
+
+mean(c(2,3,4))
+
+# this is a comment
+
+# read in the data
+
+library(here)
+
+dietdat1 <- read.csv(here("data/allwt2.csv"))
+
+head(dietdat1)
+
+tail(dietdat1)
+
+names(dietdat1)
