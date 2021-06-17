d22ee4c43c1d04cee6f80c366a7e9cf343fee5cc
sgaichas <sgaichas@gmail.com>
2021-06-17 11:42:39 -0400

    add script and visualizations

--- MyScript.R
+++ MyScript.R
@@ -1,3 +1,5 @@
+# demonstrate interactive R
+
 2+2
 
 mean(c(2,3,4))
@@ -10,8 +12,16 @@ library(here)
 
 dietdat1 <- read.csv(here("data/allwt2.csv"))
 
+# exploratory commands
+
 head(dietdat1)
 
 tail(dietdat1)
 
 names(dietdat1)
+
+# commands to visualize the data using "tidyverse"
+
+libary(tidyverse)  #for data wrangling and plotting
+
+
