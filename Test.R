d5269182547d386873bf7daedc07b7e2607b4aaf
Sarah Gaichas <sarah.gaichas@noaa.gov>
2022-07-11 14:10:54 -0400

    works without geom_gls

--- Test.R
+++ Test.R
@@ -1,22 +1,24 @@
+library(tidyverse)
+
 # importing data
-prd_prey <- read.csv("Predator Data/maCa3fin.csv")
+prd_prey <- read.csv(here::here("Predator_Data/mCa3fin.csv"))
 names(prd_prey)
 
 # plotting trends in predator-prey consumption
 plotAggPreytrend <- function(dat,prey){   #defines the name of the function and the required inputs
   
- dat %>% 
-  filter(!is.na(mean),
+  dat %>% 
+    filter(!is.na(mean),
            prey==prey) %>%          #take out the NA values (literally "not is NA")
-  ggplot(aes(x=year, y=mean)) +     #year on x axis, %diet comp on y
-  geom_point()+                               #proportion in each year is a point
-  ecodata::geom_gls(warn = FALSE) + #prints lines if significant trend
-  ecodata::theme_facet() +                   #a simpler theme, easier to read
-  facet_wrap(~svspp,                 #separate by epu, season, and prey
+    ggplot(aes(x=year, y=mean)) +     #year on x axis, %diet comp on y
+    geom_point()+                               #proportion in each year is a point
+    #ecodata::geom_gls() + #prints lines if significant trend
+    ecodata::theme_facet() +                   #a simpler theme, easier to read
+    facet_wrap(~svspp,                 #separate by epu, season, and prey
                labeller = labeller(.multi_line = FALSE),
                scales="free") +      #format labels              
-  theme(strip.text.x = element_text(size = 8)) +  #plot titles smaller
-  labs(y = "consumption")               #add sensible labels
+    theme(strip.text.x = element_text(size = 8)) +  #plot titles smaller
+    labs(y = "consumption")               #add sensible labels
   
 }  
 
