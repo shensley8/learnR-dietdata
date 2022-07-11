24412c9b4653b3ebe92b510f9e4aeda7b50bdd62
Sarah Gaichas <sarah.gaichas@noaa.gov>
2022-07-11 14:17:57 -0400

    geom_gls works with updated filter statement

--- Test.R
+++ Test.R
@@ -5,14 +5,15 @@ prd_prey <- read.csv(here::here("Predator_Data/mCa3fin.csv"))
 names(prd_prey)
 
 # plotting trends in predator-prey consumption
-plotAggPreytrend <- function(dat,prey){   #defines the name of the function and the required inputs
+plotAggPreytrend <- function(dat,py){   #defines the name of the function and the required inputs
   
   dat %>% 
-    filter(!is.na(mean),
-           prey==prey) %>%          #take out the NA values (literally "not is NA")
+    dplyr::filter(prey==py,
+                  !is.na(mean),
+           ) %>%          #take out the NA values (literally "not is NA")
     ggplot(aes(x=year, y=mean)) +     #year on x axis, %diet comp on y
     geom_point()+                               #proportion in each year is a point
-    #ecodata::geom_gls() + #prints lines if significant trend
+    ecodata::geom_gls() + #prints lines if significant trend
     ecodata::theme_facet() +                   #a simpler theme, easier to read
     facet_wrap(~svspp,                 #separate by epu, season, and prey
                labeller = labeller(.multi_line = FALSE),
@@ -22,5 +23,5 @@ plotAggPreytrend <- function(dat,prey){   #defines the name of the function and 
   
 }  
 
-plotAggPreytrend((prd_prey),"all")
+plotAggPreytrend(prd_prey,"all")
 
